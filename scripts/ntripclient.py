1c33ae435ccb8b32b15fc3f00cfc412751d602a3
Josh McCulloch <mcculloch@windowslive.com>
2023-02-27 07:53:39 +1300

    Removed threading from client. This thread could hang without killing the node. Letting the node die when something goes wrong is better because then ROS can relaunch the node.

--- scripts/ntripclient.py
+++ scripts/ntripclient.py
@@ -27,11 +27,22 @@ def patch_http_response_read(func):
     return inner
 http.client.HTTPResponse.read = patch_http_response_read(http.client.HTTPResponse.read)
 
-class ntripconnect(Thread):
-    def __init__(self, ntc):
-        super(ntripconnect, self).__init__()
-        self.ntc = ntc
-        self.stop = False
+
+
+class ntripclient:
+    def __init__(self):
+        rospy.init_node('ntripclient', anonymous=True)
+
+        self.rtcm_topic = rospy.get_param('~rtcm_topic', 'rtcm')
+        self.nmea_topic = rospy.get_param('~nmea_topic', 'nmea')
+
+        self.ntrip_server = rospy.get_param('~ntrip_server')
+        self.ntrip_user = rospy.get_param('~ntrip_user')
+        self.ntrip_pass = rospy.get_param('~ntrip_pass')
+        self.ntrip_stream = rospy.get_param('~ntrip_stream')
+        self.nmea_gga = rospy.get_param('~nmea_gga')
+
+        self.pub = rospy.Publisher(self.rtcm_topic, Message, queue_size=10)
 
     def run(self):
 
@@ -39,10 +50,10 @@ class ntripconnect(Thread):
             'Ntrip-Version': 'Ntrip/2.0',
             'User-Agent': 'NTRIP ntrip_ros',
             'Connection': 'close',
-            'Authorization': 'Basic ' + b64encode((self.ntc.ntrip_user + ':' + str(self.ntc.ntrip_pass)).encode("utf-8")).decode("utf-8")
+            'Authorization': 'Basic ' + b64encode((self.ntrip_user + ':' + str(self.ntrip_pass)).encode("utf-8")).decode("utf-8")
         }
-        connection = HTTPConnection(self.ntc.ntrip_server)
-        connection.request('GET', '/'+self.ntc.ntrip_stream, self.ntc.nmea_gga, headers)
+        connection = HTTPConnection(self.ntrip_server)
+        connection.request('GET', '/'+self.ntrip_stream, self.nmea_gga, headers)
         # Patch the response class to work with ublox ntrip server.
         connection.response_class = NTRIPResponse
         response = connection.getresponse()
@@ -50,18 +61,7 @@ class ntripconnect(Thread):
         buf = bytes()
         rmsg = Message()
         restart_count = 0
-        while not self.stop:
-            '''
-            data = response.read(100)
-            pos = data.find('\r\n')
-            if pos != -1:
-                rmsg.message = buf + data[:pos]
-                rmsg.header.seq += 1
-                rmsg.header.stamp = rospy.get_rostime()
-                buf = data[pos+2:]
-                self.ntc.pub.publish(rmsg)
-            else: buf += data
-            '''
+        while(not rospy.is_shutdown()):
 
             ''' This now separates individual RTCM messages and publishes each one on the same topic '''
             data = response.read(1)
@@ -82,7 +82,7 @@ class ntripconnect(Thread):
                     rmsg.message = buf
                     rmsg.header.seq += 1
                     rmsg.header.stamp = rospy.get_rostime()
-                    self.ntc.pub.publish(rmsg)
+                    self.pub.publish(rmsg)
                     buf = bytes()
                 else: 
                     #print (data)
@@ -92,37 +92,13 @@ class ntripconnect(Thread):
                 restart_count = restart_count + 1
                 rospy.logwarn(f"Zero length {restart_count}")
                 connection.close()
-                connection = HTTPConnection(self.ntc.ntrip_server)
-                connection.request('GET', '/'+self.ntc.ntrip_stream, self.ntc.nmea_gga, headers)
+                connection = HTTPConnection(self.ntrip_server)
+                connection.request('GET', '/'+self.ntrip_stream, self.nmea_gga, headers)
                 response = connection.getresponse()
                 if response.status != 200: raise Exception("Unexcpted http resopnse code: {response.status}")
                 buf = ""
 
         connection.close()
-
-class ntripclient:
-    def __init__(self):
-        rospy.init_node('ntripclient', anonymous=True)
-
-        self.rtcm_topic = rospy.get_param('~rtcm_topic', 'rtcm')
-        self.nmea_topic = rospy.get_param('~nmea_topic', 'nmea')
-
-        self.ntrip_server = rospy.get_param('~ntrip_server')
-        self.ntrip_user = rospy.get_param('~ntrip_user')
-        self.ntrip_pass = rospy.get_param('~ntrip_pass')
-        self.ntrip_stream = rospy.get_param('~ntrip_stream')
-        self.nmea_gga = rospy.get_param('~nmea_gga')
-
-        self.pub = rospy.Publisher(self.rtcm_topic, Message, queue_size=10)
-
-        self.connection = None
-        self.connection = ntripconnect(self)
-        self.connection.start()
-
-    def run(self):
-        rospy.spin()
-        if self.connection is not None:
-            self.connection.stop = True
 
 if __name__ == '__main__':
     c = ntripclient()
